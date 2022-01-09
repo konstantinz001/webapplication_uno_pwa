bf551af2468f265685f3e56394016f514d5dddd7
Konstantin Zabaznov <konsi.zabaznov@web.de>
2022-01-09 22:22:08 +0100

    .

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/webapplication_uno_pwa/'
+      : '/',
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    https: true
+  }
 }
