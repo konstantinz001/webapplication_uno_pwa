0353ae92cb89197d183a907e5729d3fcb3b6a0e9
Konstantin Zabaznov <konsi.zabaznov@web.de>
2022-01-09 21:55:54 +0100

    .

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init 
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:konstantinz001/webapplication_uno_pwa.git master:Github-Pages
+git push -f git@github.com:konstantinz001/webapplication_uno_pwa.git main:Github-Pages
 
 cd -
